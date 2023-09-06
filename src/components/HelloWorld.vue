538ebfe0ab79a5d6fbc1ff740f29f7c23e0c7ff2
tiennx <tiennx@vmogroup.com>
2023-09-06 11:26:15 +0700

    init project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { reactive } from "vue"
+
+const state = reactive({})
+</script>
+
+<template>
+    <div>Hello World</div>
+    <div>It works, nice!</div>
+    {{ state }}
+</template>
+
+<style scoped lang="scss">
+</style>
